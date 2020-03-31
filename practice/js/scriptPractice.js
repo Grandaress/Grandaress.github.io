3fc67255b100b7316026babd6e562ea55f99f4b9
Grandaress <aress.artem@gmail.com>
2020-03-31 18:01:41 +0300

    improve response and add js

--- practice/js/scriptPractice.js
+++ practice/js/scriptPractice.js
@@ -15,5 +15,78 @@ $(function(){
                 items:1
             }
         }
-    })
+    });
+
+    $('.project1').on({
+        mouseover: function(){
+            $('.desc1').addClass('show-portfolio');
+        },
+        mouseleave: function(){
+            $('.desc1').removeClass('show-portfolio');
+        },
+    });
+
+    $('.project2').on({
+        mouseover: function(){
+            $('.desc2').addClass('show-portfolio');
+        },
+        mouseleave: function(){
+            $('.desc2').removeClass('show-portfolio');
+        },
+    });
+
+    $('.project3').on({
+        mouseover: function(){
+            $('.desc3').addClass('show-portfolio');
+        },
+        mouseleave: function(){
+            $('.desc3').removeClass('show-portfolio');
+        },
+    });
+
+    $('.project4').on({
+        mouseover: function(){
+            $('.desc4').addClass('show-portfolio');
+        },
+        mouseleave: function(){
+            $('.desc4').removeClass('show-portfolio');
+        },
+    });
+
+    $('.project5').on({
+        mouseover: function(){
+            $('.desc5').addClass('show-portfolio');
+        },
+        mouseleave: function(){
+            $('.desc5').removeClass('show-portfolio');
+        },
+    });
+
+    $('.project6').on({
+        mouseover: function(){
+            $('.desc6').addClass('show-portfolio');
+        },
+        mouseleave: function(){
+            $('.desc6').removeClass('show-portfolio');
+        },
+    });
+
+    $('.project7').on({
+        mouseover: function(){
+            $('.desc7').addClass('show-portfolio');
+        },
+        mouseleave: function(){
+            $('.desc7').removeClass('show-portfolio');
+        },
+    });
+
+    $('.project8').on({
+        mouseover: function(){
+            $('.desc8').addClass('show-portfolio');
+        },
+        mouseleave: function(){
+            $('.desc8').removeClass('show-portfolio');
+        },
+    });
+
 });
